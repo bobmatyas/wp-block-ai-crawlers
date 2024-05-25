8e91cda0ed62a3ba24b914547a0d0b6b65452fe4
bob <45246438+bobmatyas@users.noreply.github.com>
2024-05-24 20:08:16 -0400

    fix formatting issues

--- inc/settings.php
+++ inc/settings.php
@@ -2,7 +2,7 @@
 /**
  * Configures settings display in admin view for plugin.
  *
- * @package "Block_AI_Crawlerss"
+ * @package "Block_AI_Crawlers"
  */
 
 add_action( 'admin_menu', 'block_ai_crawlers_add_settings_menu' );
@@ -41,6 +41,6 @@ function block_ai_crawlers_add_settings_menu() {
  * @return void
  */
 function block_ai_crawlers_option_page() {
-	$settings_HTML = file_get_contents( plugins_url( 'settings-html.php' , __FILE__ ));
+	$settings_HTML = file_get_contents( plugins_url( 'settings-content.php' , __FILE__ ));
 	echo $settings_HTML;
 }
